02d941629052d1ed076f38ae2f0f20e93914699a
kristofpaulusz <paulusz.kristof.csanad@diak.szamalk-szalezi.hu>
2022-05-06 12:52:13 +0200

    működöképes

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
 window.addEventListener("load", init);
 
+class Card {
+  constructor(id, src, isFlipped) {
+    this.id = id;
+    this.src = src;
+    this.isFlipped = isFlipped;
+  }
+}
+
+let objects = [];
+
+let FlippedCards = [];
 
 function ID(elem) {
-    return document.getElementById(elem);
+  return document.getElementById(elem);
 }
+
 // Ez a függvény egy plagizáció
 function shuffle(array) {
   let currentIndex = array.length,
@@ -24,34 +36,82 @@ function shuffle(array) {
 
   return array;
 }
+
 function kepek() {
-    let txt= "";
+  let txt = "";
   let srcs = [];
   for (let index = 0; index < 20; index++) {
     srcs.push("kepek/kep" + (index + 1) + ".jpg");
     srcs.push("kepek/kep" + (index + 1) + ".jpg");
   }
   srcs = shuffle(srcs);
-  console.log(srcs);
-  
+
   for (let index = 0; index < 40; index++) {
-    txt += '<div class="'+"card"+'" id="'+index+'"><div class="front"><img src="kepek/hatter.jpg" alt=""></div><div class="back"><img src="'+srcs[index]+'" alt=""></div></div>'
+    objects.push(new Card(index, srcs[index], false));
+    txt +=
+      '<div class="' +
+      "card" +
+      '" id="' +
+      index +
+      '"><div class="front"><img src="kepek/hatter.jpg" alt=""></div><div class="back"><img src="' +
+      srcs[index] +
+      '" alt=""></div></div>';
   }
   document.getElementById("board").innerHTML = txt;
-  
-  
-  
-  return txt;
+}
+
+function isThereAWin() {
+  let mindenOke = true;
+  let index = 0;
+  while (mindenOke && index < 40) {
+    if (!objects[index].isFlipped) {
+      mindenOke = false;
+    }
+    index += 1;
+  }
+  return mindenOke;
 }
 function toggle() {
-    this.classList.toggle("flipCard")
-}
-function forditas() {
-    for (let index = 0; index < 40; index++) {
-        ID(index).addEventListener("click", toggle)
+  objects[this.id].isFlipped = true;
+  FlippedCards.push(objects[this.id]);
+  this.classList.toggle("flipCard");
+  this.removeEventListener("click", toggle);
+  if (FlippedCards.length == 2) {
+    if (FlippedCards[0].src == FlippedCards[1].src) {
+      FlippedCards = [];
+    } else {
+      FlippedCards.forEach((element) => {
+        setTimeout(() => {
+          ID(element.id).classList.toggle("flipCard");
+          ID(element.id).addEventListener("click", toggle);
+          objects[element.id].isFlipped = false;
+        }, 700);
+      });
+
+      FlippedCards = [];
     }
+  }
+  if(isThereAWin()) {
+    document.getElementById("title").innerHTML = "<h1><span>Na</span><span>gy</span><span>on </span><span>Sz</span><span>ép </span><span>Gy</span><span>őz</span><span>el</span><span>em!</span></h1>"
+    let sizes = ["small", "smaller", "large", "larger"]
+    let spans = document.querySelectorAll("h1 span")
+    setInterval(() => {
+      spans[Math.floor(Math.random()*spans.length)].style.color = "RGB("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")";
+      spans[Math.floor(Math.random()*spans.length)].style.fontSize = sizes[Math.floor(Math.random()*sizes.length)];
+      
+    }, 200)
+    
+    
+  };
+}
+
+function forditas() {
+  for (let index = 0; index < 40; index++) {
+    ID(index).addEventListener("click", toggle);
+  }
 }
 function init() {
-    kepek();
-    forditas();
+  kepek();
+  forditas();
+  
 }
